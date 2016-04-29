9c21fc3fef633f7f9be4c64e4e212d72ce0f71d6
John Sietsma <john.sietsma@gmail.com>
2016-04-29 11:26:18 +1000

    Adding the diagonal difference challenge.

---
+++ DiagonalDifference.cpp
@@ -0,0 +1,61 @@
+/*
+    https://www.hackerrank.com/challenges/diagonal-difference
+    Given a square matrix of size N×NN×N, calculate the absolute difference between the sums of its diagonals.
+*/
+
+#include <assert.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+int string_to_int( const string& str )
+{
+    stringstream stream(str);
+    int res;
+    return (stream >> res) ? res : 0;
+}
+
+
+int main() {
+    size_t matrixSize;
+    
+    // The first line is the number of ints
+    cin >> matrixSize;
+
+    // Our running count    
+    int diagonalSum1 = 0;
+    int diagonalSum2 = 0;
+    
+    // The current index of hte diagonals within the row
+    size_t diagonalIndex1 = 0;
+    size_t diagonalIndex2 = matrixSize-1;
+    
+    // Working buffer for the numbers within a row
+    vector<int> rowNumbers;
+    rowNumbers.reserve(matrixSize);
+    
+    for(size_t rowIndex=0; rowIndex<matrixSize; rowIndex++) {
+        rowNumbers.clear();
+        // Extract all the numbers in the row
+        for(size_t colIndex=0; colIndex<matrixSize; colIndex++) {
+            string numString;
+            cin >> numString;
+            rowNumbers.push_back( string_to_int(numString) );
+        }
+
+        assert( diagonalIndex1 < matrixSize );
+        assert( diagonalIndex2 >= 0 );
+        
+        // Grab the correct numbers and sum them
+        diagonalSum1 += rowNumbers[diagonalIndex1++];
+        diagonalSum2 += rowNumbers[diagonalIndex2--];
+    }
+
+    cout << abs(diagonalSum1-diagonalSum2) << endl;
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -3,11 +3,16 @@ CFLAGS=-c -Wall -g -std=c++11 -stdlib=libc++
 LDFLAGS=
 OBJECTS=$(SOURCES:.cpp=.o)
 
+
+OBJECTS=helpers.o
 LONELY_OBJECTS=LonelyInteger.o
 SUM_OBJECTS=SimpleArraySum.o
 SUM_OBJECTS=VeryBigArraySum.o
+DIAGONAL_DIFFERENCE_OBJECTS=DiagonalDifference.o
 
-all: LonelyTest SimpleArraySumTest VeryBigArraySumTest
+BINARIES=LonelyTest SimpleArraySumTest VeryBigArraySumTest DiagonalDifferenceTest
+
+all: $(BINARIES)
 
 # Lonely Integer
 LonelyTest: LonelyInteger
@@ -35,5 +40,16 @@ VeryBigArraySum: $(SUM_OBJECTS)
 	$(CC) $(LDFLAGS) $(SUM_OBJECTS) -o $@
 
 
+# Diagonal Difference
+DiagonalDifferenceTest: DiagonalDifference
+	./DiagonalDifference < input/diagonal_difference1.txt
+
+DiagonalDifference: $(DIAGONAL_DIFFERENCE_OBJECTS)
+	$(CC) $(LDFLAGS) $(DIAGONAL_DIFFERENCE_OBJECTS) -o $@
+
+
 .cpp.o:
 	$(CC) $(CFLAGS) $< -o $@
+
+clean:
+	rm $(BINARIES) *.o
